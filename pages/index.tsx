fbceead216ca8b5f0acb0d1cc02beac163f2c531
Wangbaoqi <wangbaoqi8839@gmail.com>
2022-06-17 10:18:12 +0800

    feat: create next image

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>nate blog images</title>
+        <meta name="description" content="nate blog images" />
+        <link rel="icon" href="/favicon.webp" />
+      </Head>
+    </div>
+  )
+}
+
+export default Home
